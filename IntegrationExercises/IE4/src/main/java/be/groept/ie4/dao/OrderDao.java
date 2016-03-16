49507c06c919f6b5b11c00ff3af3113e34a68183
Koen Serneels <koen.serneels@hpe.com>
2016-03-16 12:14:46 +0100

    Adding IE4

---
+++ IntegrationExercises/IE4/src/main/java/be/groept/ie4/dao/OrderDao.java
@@ -0,0 +1,6 @@
+package be.groept.ie4.dao;
+
+public interface OrderDao {
+
+	// TODO complete me
+}
---
+++ IntegrationExercises/IE4/src/main/java/be/groept/ie4/entities/Product.java
@@ -0,0 +1,6 @@
+package be.groept.ie4.entities;
+
+public class Product {
+
+	// TODO complete me
+}
