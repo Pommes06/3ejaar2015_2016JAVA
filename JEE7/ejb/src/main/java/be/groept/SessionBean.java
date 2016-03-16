c6a614f50de9ece75cda5eba994949145f14e5d8
Koen Serneels <koen.serneels@hpe.com>
2016-03-16 13:39:14 +0100

    Adding JEE7

---
+++ JEE7/ejb/src/main/java/be/groept/SessionBean.java
@@ -0,0 +1,12 @@
+package be.groept;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by koen on 16/03/16.
+ */
+@Stateless(name = "SessionEJB")
+public class SessionBean {
+	public SessionBean() {
+	}
+}
